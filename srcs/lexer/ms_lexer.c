7fbc9ab65f165a8b9fde456ea16d5c7b7ee51999
Yik Wai Chan <yichan@u91z06s03.42kl.edu.my>
2023-03-26 05:21:26 +0800

    add lexer

--- srcs/lexer/ms_lexer.c
+++ srcs/lexer/ms_lexer.c
@@ -6,7 +6,7 @@
 /*   By: yichan <yichan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 01:57:02 by yichan            #+#    #+#             */
-/*   Updated: 2023/03/26 04:18:28 by yichan           ###   ########.fr       */
+/*   Updated: 2023/03/26 05:20:33 by yichan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,27 @@
 
 int	ms_lexer(t_book *record)
 {
-	t_list	*token_itr;
-	t_token *token_ptr;
-	char	*check;
-	const t_token	arr[] = {
-	{.entity = "|", .type = PIPE},
-	{.entity = "?", .type = QUERY},
-	{.entity = "$", .type = DOLLAR},
-	{.entity = "<", .type = RIGHT},
-	{.entity = ">", .type = LEFT},
-	};
+	t_list			*token_itr;
+	t_token			*token_ptr;
+	char			*check;
+	const t_token	arr[] = {{.entity = "|", .type = PIPE},
+	{.entity = "?", .type = QUERY}, {.entity = "$", .type = DOLLAR},
+	{.entity = "<", .type = LEFT}, {.entity = ">", .type = RIGHT}};
+	int				i;
 
-	token_itr = record->token;
+	token_itr = (t_list *)record->token;
 	while (token_itr)
 	{
-		token_ptr = (t_token *)token_itr->content;
-		check = ((t_token *)token_itr->content)->entity;
+		i = -1;
+		token_ptr = ((t_token *)token_itr->content);
+		check = token_ptr->entity;
 		token_ptr->type = LITERAL;
-		for (size_t i = 0; i < (sizeof(arr) / sizeof(arr[0])); i++)
+		while (++i < (int)(sizeof(arr) / sizeof(arr[0])))
 		{
-			if (!ft_strcmp(arr[i].entity, check))
+			if ((ft_strcmp(arr[i].entity, check)) == 0)
 				token_ptr->type = arr[i].type;
 		}
-		printf("token:%s type:%d\n", ((t_token *)token_itr->content)->entity, token_ptr->type);
+		printf("token_ptr: %s type: %d \n", token_ptr->entity, token_ptr->type);
 		token_itr = token_itr->next;
 	}
 	return (0);
